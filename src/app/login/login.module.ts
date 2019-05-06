c8802ce6250bc0c54840cb3b825a22ac5579b0a3
Jason Franco Quintero <jada183.jf@gmail.com>
2019-05-06 10:19:36 +0200

    login with material

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,11 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 @NgModule({
-  imports: [CommonModule,
-    FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatTabsModule,
+    MatCardModule,
+    MatProgressSpinnerModule
+  ],
   declarations: [LoginComponent],
-  exports: [LoginComponent],
-
+  exports: [LoginComponent]
 })
 export class LoginModule {}
